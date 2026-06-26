0df76656ddb5e47e66d5b438943de8cb4500432a
Leosss24 <leomartinezubal@gmail.com>
2026-06-26 18:16:42 +0200

    Update Supabase client URL and key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 export const supabase = createClient(
-  'https://rfrcwfbwhknzmhsjbjle.supabase.co',  // ← cambia por tu URL
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmcmN3ZmJ3aGtuem1oc2piamxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NTg4NjcsImV4cCI6MjA2NjUzNDg2N30.XBGFUagLFnicFATNR8yU19aoxuEU70KM08cAMryD35w'                      // ← cambia por tu clave
+  'https://jsuiofuizkmgpmfwypry.supabase.co',  // ← cambia por tu URL
+  'sb_publishable_WSvo8Sqh0Jl20m6EVYO_KQ_jSjEkQG9'                      // ← cambia por tu clave
 );
